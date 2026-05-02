a66ef3c51a4878e8f62e2e979638bcac38ba978e
ibeO-GH <ibe.okorafor@gmail.com>
2026-05-02 13:29:42 +0100

    fix: render port config

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.put("/tasks/reorder", async (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
